51861fba555942df2099998641e3ba411dfaccb0
paichiwo <lzerucha@gmail.com>
2023-05-14 22:22:53 +0100

    Change format_date function to format dates from API like this: 'Sunday, 14 May'.

--- new_app.py
+++ new_app.py
@@ -1,18 +1,28 @@
 import requests
 from datetime import datetime
 import geocoder
+import calendar
 from tkinter import *
 from tkinter import ttk, messagebox
 import tkinter as tk
 
 # Paste you API key from https://weatherapi.com/ here:
-api_key = "58426b1c8989446e9b7142031230103"
+api_key = ""
+
+# info for precipitation:
+# - Light rain gives up to 2–4 mm (0.07–0.15 in)
+# - Moderate rain gives 5–6 mm (0.19–0.23 in)
+# - Rain or strong rain gives up about 15–20 mm (0.59–0.78 in)
+# - Rainfall gives more than 30 mm (1.18 in)
 
 
 def format_date(date):
-    """ Format date to european format """
+    """ Format date to weekday, day month_name (Monday, 14 May) """
     date_object = datetime.strptime(date, "%Y-%m-%d")
-    return date_object.strftime("%d/%m/%y")
+    week_day = calendar.day_name[date_object.weekday()]
+    month = date_object.month
+    month_name = calendar.month_name[month]
+    return f"{week_day[:3]}, {date_object.day} {month_name}"
 
 
 # root = Tk()
@@ -36,24 +46,22 @@ else:
 url = f"http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={location}&days=5&aqi=no&alerts=no"
 response = requests.get(url)
 data = response.json()
-print(data)
 
 # Info data
 city = data['location']['name']
 region = data['location']['region']
 country = data['location']['country']
-local_time = format_date(data['location']['localtime'][:-6]) + data['location']['localtime'][10:]
-print(local_time)
+local_time = format_date(data['location']['localtime'][:-6])
 
 # Current data
-current_is_day = data['current']['is_day']
 current_temp = data['current']['temp_c']
 current_condition = data['current']['condition']['text']
 current_feelslike = data['current']['feelslike_c']
 current_wind_speed = data['current']['wind_kph']
 current_humidity = data['current']['humidity']
-current_precipitation = data['current']['precip_mm'] # mm instead of chance of rain %
+current_precipitation = data['current']['precip_mm']  # mm, instead of chance of rain %
 current_pressure = data['current']['pressure_mb']
+current_is_day = data['current']['is_day']
 
 # Forecast data
 forecast = data['forecast']['forecastday']
@@ -73,4 +81,3 @@ for date_entry in forecast:
         "avg_humidity": forecast_avg_humidity,
         "condition": forecast_condition,
     })
-
