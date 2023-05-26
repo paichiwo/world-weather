15e3709b84da548de55b97637bb767ea4baccdb8
k1341382 <84815698+k1341382@users.noreply.github.com>
2023-05-25 21:05:23 -0500

    Add files via upload

--- world_weather.py
+++ world_weather.py
@@ -13,10 +13,13 @@ errors = {"API_404": 'API key not valid, or not yet activated. If you recently s
 l_blue = '#1581ef'
 d_blue = '#1167f2'
 
+
 # https://weatherbit.io/ API KEY
 api_key = ''
 
-
+def read_api_key():
+    with open("api_key.txt") as f:
+        return f.read()
 def format_date_long(date):
     """Format date to weekday, day month_name (Monday, 14 May)."""
     date_object = datetime.strptime(date, '%Y-%m-%d')
@@ -177,6 +180,7 @@ def world_weather():
         day_3_icon.config(file=icons_mini[forecast_data_list[2][4]])
 
     def get_weather():
+        global api_key #maybe later we should consider classes?
         """Connect to API, get data and update tkinter labels."""
         # If textfield left empty use user current location based on IP address
         current_data = {}
@@ -184,6 +188,8 @@ def world_weather():
             location = textfield.get()
         else:
             location = get_user_location()
+        if not api_key.strip():#just in case...
+            api_key = read_api_key()# better access
         # Weather data flow
         response = get_response_code(api_key, location)
         if response == 200:
@@ -236,7 +242,7 @@ def world_weather():
     condition = Label(text='', font=('Noto Sans', 11), justify='center', bg=l_blue, fg='white', width=27)
     condition.place(x=53, y=390, height=30)
     date_info = Label(text='', font=('Noto Sans', 8), justify='center', bg=l_blue, fg='white', width=30)
-    date_info.place(x=69, y=418, height=15)
+    date_info.place(x=84, y=418, height=15)
     paichiwo = Label(text='by paichiwo ', font=('Noto Sans', 8), bg=l_blue, fg='white')
     paichiwo.place(x=143, y=300)
 
